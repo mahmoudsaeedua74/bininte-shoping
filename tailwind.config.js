942f2e3197a0055b7668648e01bb418419a9dde2
mahmoudsaeedua74 <mahmoudsaeed0112074@gmail.com>
2025-01-24 02:05:29 +0200

    "tailwind "

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      colors: {
+        textColor: "#22262A",
+        mainColor: "#33A0FF",
+        secondColor: "#C1C8CE",
+        salmonColor: "#FF4858",
+        borderColor: "#F6F7F8",
+      },
+    },
+  },
+  plugins: [],
+} 
+
